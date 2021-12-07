4f49d620f191cd22f16311c4499220ec51d5de7a
AdamLyhsx <adamly@vip.qq.com>
2021-12-07 23:28:00 +0800

    springboot升级实验

--- src/main/java/com/adamly/xin6/device/converter/SexConverter.java
+++ src/main/java/com/adamly/xin6/device/converter/SexConverter.java
@@ -1,22 +1,22 @@
-package com.adamly.xin6.device.converter;
-
-import com.adamly.xin6.pojo.SexEnum;
-
-import javax.persistence.AttributeConverter;
-
-/**
- * @author adamly
- * @version 1.0
- * @date 2020/3/22 17:02
- */
-public class SexConverter implements AttributeConverter<SexEnum,Integer> {
-    @Override
-    public Integer convertToDatabaseColumn(SexEnum sexEnum) {
-        return sexEnum.getId();
-    }
-
-    @Override
-    public SexEnum convertToEntityAttribute(Integer id) {
-        return SexEnum.getEnumById(id);
-    }
-}
+//package com.adamly.xin6.device.converter;
+//
+//import com.adamly.xin6.pojo.SexEnum;
+//
+//import javax.persistence.AttributeConverter;
+//
+///**
+// * @author adamly
+// * @version 1.0
+// * @date 2020/3/22 17:02
+// */
+//public class SexConverter implements AttributeConverter<SexEnum,Integer> {
+//    @Override
+//    public Integer convertToDatabaseColumn(SexEnum sexEnum) {
+//        return sexEnum.getId();
+//    }
+//
+//    @Override
+//    public SexEnum convertToEntityAttribute(Integer id) {
+//        return SexEnum.getEnumById(id);
+//    }
+//}
